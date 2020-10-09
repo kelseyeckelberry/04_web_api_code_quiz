88eee9b955d63356b12b647e217623dba835bec7
kelseck <kelseyeckelberry@gmail.com>
2020-10-09 00:04:43 -0500

    Initial Javascript

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-.createElement
-.setAttribute
-.appendChild
+var timerEl = document.querySelector("#timer");
+var startBtn = document.querySelector("#start-btn");
+
+
+// Array of questions & answers for quiz.
+var questions = [
+    { q: "Commonly used data types DO NOT include:", a: "3. alerts"},
+    { q: "The condition in an if / else statement is enclosed within _______.", a: "3. parentheses"},
+    { q: "Arrays in Javascript can be used to store ______.", a: "4. all of the above"},
+    { q: "String values must be enclosed within _____ when being assigned to variables.", a: "3. quotes"},
+    { q: "A very useful tool used during development and debugging for printing content to the debugger is:", a: "4. console.log"},
+];
+
+//Timer Element
+
+
+// Loop over all question objects.
+for (var i = 0; i < questions.length; i++) {
+
+}
